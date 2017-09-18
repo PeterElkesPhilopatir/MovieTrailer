016bb67ab72450a0253023a7e2d4d7ccbdc277ad
PeterElkesPhilopatir <petershawki4@gmail.com>
2017-09-18 20:38:54 +0200

    Update media.py

--- media.py
+++ media.py
@@ -3,12 +3,13 @@ import webbrowser
 
 class Movie():
 
-    def __init__(self,title,story,poster_image_url,trailer_youtube_url):
+    def __init__(self, title, story, poster_image_url, trailer_youtube_url):
+        # to Construct the movie object
         self.title = title
         self.story = story
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
-
     def showTrailer(self):
+        # it opens browser to show the link
         webbrowser.open(self.trailer)
